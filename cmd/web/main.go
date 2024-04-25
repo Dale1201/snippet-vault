46960d8fc6bee6c1771f8e57d18c8d863e28e659
Dale Beniga <dalecd9@gmail.com>
2024-04-25 22:21:05 +1000

    create error helpers to reduce repeated code, moved route logic to routes file for seperation of concerns

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -26,18 +26,11 @@ func main() {
 		infoLog: infoLog,
 	}
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet", app.showSnippet)
-	mux.HandleFunc("/snippet/create", app.createSnippet)
-
-	fileServer := http.FileServer(http.Dir("../../ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
+	// Creating and Running HTTP server
 	srv := &http.Server{
 		Addr: *addr,
 		ErrorLog: errorLog,
-		Handler: mux,
+		Handler: app.routes(),
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
