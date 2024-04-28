79b4e3d9025c78520a578831948062b02f21f15e
Dale Beniga <dalecd9@gmail.com>
2024-04-28 19:22:56 +1000

    remove commented out code

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -9,12 +9,7 @@ import (
 
 func (app *application) routes() http.Handler {
 	standardMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
-
-	// mux := http.NewServeMux()
-	// mux.HandleFunc("/", app.home)
-	// mux.HandleFunc("/snippet", app.showSnippet)
-	// mux.HandleFunc("/snippet/create", app.createSnippet)
-
+	
 	mux := pat.New()
 	mux.Get("/", http.HandlerFunc(app.home))
 	mux.Get("/snippet/create", http.HandlerFunc(app.createSnippetForm))
