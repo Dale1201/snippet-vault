eb3cdbd0c38edc378be6653ea2d4a84cef1219ae
Dale Beniga <dalecd9@gmail.com>
2024-05-01 12:22:04 +1000

    start adding user login functionality

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -21,6 +21,7 @@ type application struct {
 	infoLog *log.Logger
 	session *sessions.Session
 	snippets *mysql.SnippetModel
+	users *mysql.UserModel
 	templateCache map[string]*template.Template
 }
 
@@ -58,6 +59,7 @@ func main() {
 		infoLog: infoLog,
 		session: session,
 		snippets: &mysql.SnippetModel{DB: db},
+		users: &mysql.UserModel{DB: db},
 		templateCache: templateCache,
 	}
 
